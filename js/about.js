7a0165755cac67df92bdd88d3cf2c049831e305b
Julianna Nacorda <jmnacord@uci.edu>
2025-10-27 13:07:20 -0700

    displayed the earliest and latest Tweets in dataset in Weekday, Month, Day, Year format YESSSS

--- js/about.js
+++ js/about.js
@@ -8,6 +8,29 @@ function parseTweets(runkeeper_tweets) {
 	tweet_array = runkeeper_tweets.map(function(tweet) {
 		return new Tweet(tweet.text, tweet.created_at);
 	});
+
+	// adding date-formatting code for finding the earliest and latest tweet. updating the spans here:
+
+	// here we collect the times from tweets
+	const times = tweet_array.map(t => t.time.getTime());
+
+	// date format:
+	const dateOptions = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'}; // Ex: Monday, January 18, 2021
+	// weekday long gives full weekday name
+	// month long gives full month name
+
+	if (times.length > 0) {
+		const earliestTwt = new Date(Math.min(...times));
+		const latestTwt = new Date(Math.max(...times));
+
+		document.getElementById('firstDate').innerText = earliestTwt.toLocaleDateString(undefined, dateOptions); // use defined so weekday and month are localizeedddd
+
+		document.getElementById('lastDate').innerText = latestTwt.toLocaleDateString(undefined, dateOptions);
+
+	} else {
+		document.getElementById('firstDate').innerText = 'N/A';
+		document.getElementById('lastDate').innerText = 'N/A';
+	}
 	
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
